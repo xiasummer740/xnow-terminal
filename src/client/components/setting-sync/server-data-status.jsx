053cb2b7a7311e5a8cef6582e960ef6c7128f8a3
xiasummer740 <xiasummer740@users.noreply.github.com>
2026-06-12 13:20:40 +0800

    fix: 同步状态面板版本字段名不匹配

--- src/client/components/setting-sync/server-data-status.jsx
+++ src/client/components/setting-sync/server-data-status.jsx
@@ -54,7 +54,7 @@ export default function ServerDataStatus (props) {
   function renderStatus () {
     const {
       lastSyncTime,
-      xnowVersion,
+      electermVersion,
       deviceName = 'unknown'
     } = status
 
@@ -64,7 +64,7 @@ export default function ServerDataStatus (props) {
         <b className='mg1r'>{dayjs(lastSyncTime).format('YYYY-MM-DD HH:mm:ss')}</b>
         <span className='mg1r'>{e('from')}:</span>
         <b className='mg1r'>{deviceName}</b>
-        <b className='mg1r'>(v{xnowVersion})</b>
+        <b className='mg1r'>(v{electermVersion})</b>
         {renderReloadButton()}
       </p>
     )
